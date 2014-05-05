757b760d30e782ef6f8654fcfdc02211588c4bec
thinkluffy <thinker.luffy@gmail.com>
2014-05-05 23:18:41 +0800

    add toast effect to some pages

--- Src/js/my.js
+++ Src/js/my.js
@@ -1,7 +1,19 @@
 (function() {
+    var showToast = function(imgSrc) {
+        // alert("imgSrc=" + imgSrc);  
+        $('#toast').attr('src', imgSrc);
+        $('#toast').finish();
+        $('#toast').fadeIn(200, function() {
+            $('#toast').fadeOut(1500);
+        });
+    };
+    
+    window.showToast = showToast;
+})();
 
+(function() {
     var panelShown = false;
-    var p1_updownButtonClicked = function() {
+    var p11_updownButtonClicked = function() {
         if (panelShown) {
             // to hide
             panelShown = false;
@@ -9,13 +21,13 @@
 
             $("#p11_panel").animate({
                 "bottom": "-80px"
-            }, 500, "linear", function() {
+            }, 300, "linear", function() {
                 // move_top();//call function to move top
             });
 
             $("#p11_updownButton").animate({
                 "bottom": "10px"
-            }, 500, "linear", function() {
+            }, 300, "linear", function() {
                 // move_top();//call function to move top
             });
 
@@ -25,17 +37,46 @@
 
             $("#p11_panel").animate({
                 "bottom": "0px"
-            }, 500, "linear", function() {
+            }, 300, "linear", function() {
                 // move_top();//call function to move top
             });
 
             $("#p11_updownButton").animate({
                 "bottom": "90px"
-            }, 500, "linear", function() {
+            }, 300, "linear", function() {
                 // move_top();//call function to move top
             });
         }
     };
 
-    window.p1_updownButtonClicked = p1_updownButtonClicked;
+    window.p11_updownButtonClicked = p11_updownButtonClicked;
+})();
+
+(function() {
+    var panelShown = false;
+    var p8_updownButtonClicked = function() {
+        if (panelShown) {
+            // to hide
+            panelShown = false;
+            // alert('a');
+
+            $("#keyboard_panel").animate({
+                "bottom": "-218px"
+            }, 300, "linear", function() {
+                // move_top();//call function to move top
+            });
+            
+        } else {
+            // $('#panel').show();
+            panelShown = true;
+
+            $("#keyboard_panel").animate({
+                "bottom": "0px"
+            }, 300, "linear", function() {
+                // move_top();//call function to move top
+            });
+        }
+    };
+
+    window.p8_updownButtonClicked = p8_updownButtonClicked;
 })();
